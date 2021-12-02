6f8fa7e80980ff28ce0b2d0e1472417027e1ce5b
jorgepasco1 <jorgepascosoto@gmail.com>
2021-12-02 17:50:15 -0500

    add day 2

--- 01-10/02-dive!/main.py
+++ 01-10/02-dive!/main.py
@@ -4,6 +4,36 @@ import sys
 def run(input_file: str) -> None:
     with open(input_file) as buffer:
         input_data = buffer.read()
+        instructions = input_data.splitlines()
+
+    position = {
+        'horizontal': 0,
+        'vertical': 0
+    }
+
+    instruction_map = {
+        'forward': {
+            'direction': 'horizontal',
+            'operator': lambda x, y: x + y
+        },
+        'down': {
+            'direction': 'vertical',
+            'operator': lambda x, y: x + y
+        },
+        'up': {
+            'direction': 'vertical',
+            'operator': lambda x, y: x - y
+        }
+    }
+
+    for inst in instructions:
+        instruction_direction, steps = inst.split()
+
+        instruction = instruction_map[instruction_direction]
+        direction, operation = instruction['direction'], instruction['operator']
+        position[direction] = operation(position[direction], int(steps))
+
+    print(position['horizontal'] * position['vertical'])
 
 
 if __name__ == "__main__":
